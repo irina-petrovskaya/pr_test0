5fee617d9033a309334845e733e52d047fc61d9f
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-10-12 14:14:35 +0300

    ip: Foo1.java - property ip2 accessors

--- src/a/b/c/Foo1.java
+++ src/a/b/c/Foo1.java
@@ -14,6 +14,14 @@ public class Foo1 {
     private String ip;
     private String ip2;
 
+    public String getIp2() {
+        return ip2;
+    }
+
+    public void setIp2(String ip2) {
+        this.ip2 = ip2;
+    }
+
     public String getIp() {
         return ip;
     }
